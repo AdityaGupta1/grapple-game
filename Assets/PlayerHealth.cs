3e865783b257b9d741a4b57ddbcb78a5d0ad1c69
AdityaGupta1 <adityag@gmail.com>
2017-10-07 01:47:35 -0700

    fixed #1 (only committing ProjectSettings and Assets, fixed a number of issues within the game

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+	public bool dead = false;
+	
+	// Update is called once per frame
+	void Update()
+	{
+		if (gameObject.transform.position.y < -10 && !dead) {
+			dead = true;
+			Die();
+		}
+	}
+
+	void Die() {
+		SceneManager.LoadScene("Main");
+	}
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public int speed = 10;
+	private bool facingRight = true;
+	public int jumpPower = 1200;
+	private float moveX;
+	private bool isOnGround;
+
+	void Update()
+	{
+		PlayerMove();
+	}
+
+	void PlayerMove()
+	{
+		// controls
+		moveX = Input.GetAxis("Horizontal");
+		if (Input.GetButtonDown("Jump")) {
+			Jump();
+		}
+
+		// animations
+
+		// player direction
+		if (moveX < 0 && facingRight) {
+			FlipPlayer();
+		} else if (moveX > 0 && !facingRight) {
+			FlipPlayer();
+		}
+
+		// physics
+		Rigidbody2D rigidbody = gameObject.GetComponent<Rigidbody2D>();
+		rigidbody.velocity = new Vector2(moveX * speed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+		rigidbody.freezeRotation = true;
+		speed = 5 + (isOnGround ? 5 : 0);
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.gameObject.tag == "Terrain") {
+			isOnGround = true;
+		}
+	}
+
+	void Jump()
+	{
+		if (isOnGround) {
+			GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpPower);
+			isOnGround = false;
+		}
+	}
+
+	void FlipPlayer()
+	{
+		facingRight = !facingRight;
+		Vector2 localScale = gameObject.transform.localScale;
+		localScale.x *= -1;
+		transform.localScale = localScale;
+	}
+}
